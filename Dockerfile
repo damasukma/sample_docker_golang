f5de3d2ca99aab11cfa4b03c327e3918e9a94692
Dama Sukma <dama.sukma@detik.com>
2020-02-27 16:47:04 +0700

    hot reload

--- Dockerfile
+++ Dockerfile
@@ -7,21 +7,34 @@ RUN apk update && apk upgrade && \
 
 WORKDIR /app
 
-COPY . .    
-# RUN make engine
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o engine main.go
+COPY go.mod go.sum ./
 
-# Distribution
-FROM alpine:latest
+RUN go mod download
 
-RUN apk update && apk upgrade && \
-    apk --update --no-cache add tzdata && \
-    mkdir /app 
+RUN go get github.com/githubnemo/CompileDaemon
 
-WORKDIR /root/ 
-
-COPY --from=builder /app/engine .
+COPY . .   
 
 EXPOSE 9000
 
-CMD ["./engine"]
+ENTRYPOINT CompileDaemon --build="go build -o engine /app/main.go" --command=./engine
+
+# RUN make engine
+# RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o engine main.go
+
+
+
+# Distribution
+# FROM alpine:latest
+
+# RUN apk update && apk upgrade && \
+#     apk --update --no-cache add tzdata && \
+#     mkdir /app 
+
+# WORKDIR /root/ 
+
+# COPY --from=builder /app/engine .
+
+# EXPOSE 9000
+
+# CMD ["./engine"]
--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"core_api/controller"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello")
+	route := mux.NewRouter()
+
+	route.HandleFunc("/", controller.HelloWorld)
+
+	server := &http.Server{
+		Addr: "0.0.0.0:9000",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      route, // Pass our instance of gorilla/mux in.
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	ShutDown(server)
+
+}
+
+func ShutDown(serv *http.Server) {
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt)
+
+	<-c
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+
+	defer cancel()
+
+	serv.Shutdown(ctx)
+
+	log.Println("shutting down")
+	os.Exit(0)
+
 }
